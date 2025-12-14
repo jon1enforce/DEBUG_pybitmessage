9b91677cb34504ad7639d8806edafe365a1914f8
root <root@fst.1.1.1.1>
2025-12-14 18:00:15 +0100

    shutdown threadsafe

--- src/helper_sql.py
+++ src/helper_sql.py
@@ -17,9 +17,10 @@ SQLite objects can only be used from one thread.
 """
 
 import threading
-
+import logging
 from six.moves import queue
 
+logger = logging.getLogger('default')
 
 sqlSubmitQueue = queue.Queue()
 """the queue for SQL"""
@@ -46,19 +47,33 @@ def sqlQuery(sql_statement, *args):
     :rtype: list
     """
     assert sql_available
-    sql_lock.acquire()
-    sqlSubmitQueue.put(sql_statement)
+    if not sql_lock.acquire(timeout=30):  # Added timeout
+        logger.error('Could not acquire SQL lock after 30 seconds')
+        return []  # Return empty list instead of hanging
+    
+    try:
+        sqlSubmitQueue.put(sql_statement, timeout=10)  # Added timeout
 
-    if args == ():
-        sqlSubmitQueue.put('')
-    elif isinstance(args[0], (list, tuple)):
-        sqlSubmitQueue.put(args[0])
-    else:
-        sqlSubmitQueue.put(args)
-    queryreturn, _ = sqlReturnQueue.get()
-    sql_lock.release()
-
-    return queryreturn
+        if args == ():
+            sqlSubmitQueue.put('', timeout=10)
+        elif isinstance(args[0], (list, tuple)):
+            sqlSubmitQueue.put(args[0], timeout=10)
+        else:
+            sqlSubmitQueue.put(args, timeout=10)
+        
+        # Get result with timeout
+        try:
+            queryreturn, _ = sqlReturnQueue.get(timeout=30)
+        except queue.Empty:
+            logger.error('SQL query timed out after 30 seconds')
+            return []
+            
+        return queryreturn
+    except queue.Full:
+        logger.error('SQL submit queue is full')
+        return []
+    finally:
+        sql_lock.release()
 
 
 def sqlExecuteChunked(sql_statement, as_text, idCount, *args):
@@ -71,8 +86,12 @@ def sqlExecuteChunked(sql_statement, as_text, idCount, *args):
     if idCount == 0 or idCount > len(args):
         return 0
 
+    if not sql_lock.acquire(timeout=30):  # Added timeout
+        logger.error('Could not acquire SQL lock for chunked execute')
+        return 0
+
     total_row_count = 0
-    with sql_lock:
+    try:
         for i in range(
                 len(args) - idCount, len(args),
                 sqlExecuteChunked.chunkSize - (len(args) - idCount)
@@ -87,35 +106,59 @@ def sqlExecuteChunked(sql_statement, as_text, idCount, *args):
                     q += "CAST(? AS TEXT)"
                     if i != n - 1:
                         q += ","
-                sqlSubmitQueue.put(sql_statement.format(q))
+                sqlSubmitQueue.put(sql_statement.format(q), timeout=10)
             else:
                 sqlSubmitQueue.put(
-                    sql_statement.format(','.join('?' * len(chunk_slice)))
+                    sql_statement.format(','.join('?' * len(chunk_slice))),
+                    timeout=10
                 )
             # first static args, and then iterative chunk
             sqlSubmitQueue.put(
-                args[0:len(args) - idCount] + chunk_slice
+                args[0:len(args) - idCount] + chunk_slice,
+                timeout=10
             )
-            ret_val = sqlReturnQueue.get()
-            total_row_count += ret_val[1]
-        sqlSubmitQueue.put('commit')
+            try:
+                ret_val = sqlReturnQueue.get(timeout=30)
+                total_row_count += ret_val[1]
+            except queue.Empty:
+                logger.error('SQL chunked execute timed out')
+                break
+        sqlSubmitQueue.put('commit', timeout=10)
+    except queue.Full:
+        logger.error('SQL submit queue is full during chunked execute')
+    finally:
+        sql_lock.release()
     return total_row_count
 
 
 def sqlExecute(sql_statement, *args):
     """Execute SQL statement (optionally with arguments)"""
     assert sql_available
-    sql_lock.acquire()
-    sqlSubmitQueue.put(sql_statement)
+    if not sql_lock.acquire(timeout=30):  # Added timeout
+        logger.error('Could not acquire SQL lock for execute')
+        return 0
+    
+    try:
+        sqlSubmitQueue.put(sql_statement, timeout=10)
 
-    if args == ():
-        sqlSubmitQueue.put('')
-    else:
-        sqlSubmitQueue.put(args)
-    _, rowcount = sqlReturnQueue.get()
-    sqlSubmitQueue.put('commit')
-    sql_lock.release()
-    return rowcount
+        if args == ():
+            sqlSubmitQueue.put('', timeout=10)
+        else:
+            sqlSubmitQueue.put(args, timeout=10)
+            
+        try:
+            _, rowcount = sqlReturnQueue.get(timeout=30)
+        except queue.Empty:
+            logger.error('SQL execute timed out after 30 seconds')
+            rowcount = 0
+            
+        sqlSubmitQueue.put('commit', timeout=10)
+        return rowcount
+    except queue.Full:
+        logger.error('SQL submit queue is full')
+        return 0
+    finally:
+        sql_lock.release()
 
 
 def sqlExecuteScript(sql_statement):
@@ -130,33 +173,52 @@ def sqlExecuteScript(sql_statement):
 def sqlStoredProcedure(procName):
     """Schedule procName to be run"""
     assert sql_available
-    sql_lock.acquire()
-    sqlSubmitQueue.put(procName)
-    if procName == "exit":
-        sqlSubmitQueue.task_done()
-        sqlSubmitQueue.put("terminate")
-    sql_lock.release()
+    if not sql_lock.acquire(timeout=30):  # Added timeout
+        logger.error('Could not acquire SQL lock for stored procedure')
+        return
+    
+    try:
+        sqlSubmitQueue.put(procName, timeout=10)
+        if procName == "exit":
+            sqlSubmitQueue.put("terminate", timeout=10)
+    except queue.Full:
+        logger.error('SQL submit queue is full for stored procedure')
+    finally:
+        sql_lock.release()
 
 
 class SqlBulkExecute(object):
     """This is used when you have to execute the same statement in a cycle."""
 
     def __enter__(self):
-        sql_lock.acquire()
+        if not sql_lock.acquire(timeout=30):  # Added timeout
+            logger.error('Could not acquire SQL lock for bulk execute')
+            raise Exception('Could not acquire SQL lock')
         return self
 
     def __exit__(self, exc_type, value, traceback):
-        sqlSubmitQueue.put('commit')
-        sql_lock.release()
+        try:
+            sqlSubmitQueue.put('commit', timeout=10)
+        except queue.Full:
+            logger.error('Could not commit bulk transaction')
+        finally:
+            sql_lock.release()
 
     @staticmethod
     def execute(sql_statement, *args):
         """Used for statements that do not return results."""
         assert sql_available
-        sqlSubmitQueue.put(sql_statement)
+        try:
+            sqlSubmitQueue.put(sql_statement, timeout=10)
 
-        if args == ():
-            sqlSubmitQueue.put('')
-        else:
-            sqlSubmitQueue.put(args)
-        sqlReturnQueue.get()
+            if args == ():
+                sqlSubmitQueue.put('', timeout=10)
+            else:
+                sqlSubmitQueue.put(args, timeout=10)
+                
+            try:
+                sqlReturnQueue.get(timeout=30)
+            except queue.Empty:
+                logger.error('SQL bulk execute timed out')
+        except queue.Full:
+            logger.error('SQL submit queue is full during bulk execute')
